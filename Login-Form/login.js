afa219d85cfef5adcf58458843457b3b7537076c
maryammuhammadafzal <natiqnagori7@gmail.com>
2024-11-24 21:21:12 +0500

    updated

--- Login-Form/login.js
+++ Login-Form/login.js
@@ -1,143 +1,143 @@
 
-import { auth, signInWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail  } from "../firebase.js";
+// import { auth, signInWithEmailAndPassword, onAuthStateChanged, sendPasswordResetEmail  } from "../firebase.js";
 
 
-// ------------------------------------DOM Elements------------------------------------//
+// // ------------------------------------DOM Elements------------------------------------//
 
-let email = document.getElementById("loginEmail")
-let password = document.getElementById("loginPassword")
-let body = document.querySelector('.body');
-let signinBtn = document.querySelector('.login-btn')
-let forgetPassword = document.getElementById('forgot-password')
+// let email = document.getElementById("loginEmail")
+// let password = document.getElementById("loginPassword")
+// let body = document.querySelector('.body');
+// let signinBtn = document.querySelector('.login-btn')
+// let forgetPassword = document.getElementById('forgot-password')
 
 
-// -----------------------------login user -------------------------------------------//
-const loginUser = ()=> {
+// // -----------------------------login user -------------------------------------------//
+// const loginUser = ()=> {
  
-  // const loginEmail = email.value ;
-  // const loginPassword = password.value ;
+//   // const loginEmail = email.value ;
+//   // const loginPassword = password.value ;
 
-    signInWithEmailAndPassword(auth, email.value, password.value)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user);
-    //Successfully login 
-    const Toast = Swal.mixin({
-        toast: true,
-        position: "top-end",
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.onmouseenter = Swal.stopTimer;
-          toast.onmouseleave = Swal.resumeTimer;
-        },
-      });
-      Toast.fire({
-        icon: "success",
-        title: "Login successfully",
-      });
-    setTimeout (()=>{
-      location.href = "../Dashboard/dashboard.html"
-    },2000)
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+//     signInWithEmailAndPassword(auth, email.value, password.value)
+//   .then((userCredential) => {
+//     // Signed in 
+//     const user = userCredential.user;
+//     console.log(user);
+//     //Successfully login 
+//     const Toast = Swal.mixin({
+//         toast: true,
+//         position: "top-end",
+//         showConfirmButton: false,
+//         timer: 3000,
+//         timerProgressBar: true,
+//         didOpen: (toast) => {
+//           toast.onmouseenter = Swal.stopTimer;
+//           toast.onmouseleave = Swal.resumeTimer;
+//         },
+//       });
+//       Toast.fire({
+//         icon: "success",
+//         title: "Login successfully",
+//       });
+//     setTimeout (()=>{
+//       location.href = "../Dashboard/dashboard.html"
+//     },2000)
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
 
-      // Switch statements on error
-      switch (errorCode) {
-        case 'auth/missing-email':
-          //  if email is empty Email
-      Swal.fire({
-        icon: "error",
-        title: "Null",
-        text: "Email is required",
-      });
-      break;
+//       // Switch statements on error
+//       switch (errorCode) {
+//         case 'auth/missing-email':
+//           //  if email is empty Email
+//       Swal.fire({
+//         icon: "error",
+//         title: "Null",
+//         text: "Email is required",
+//       });
+//       break;
 
-      //  if email is empty Email
-      case 'auth/invalid-email':
-      Swal.fire({
-        icon: "error",
-        title: "Error",
-        text: "Email is Incorrect",
-      });
-      email.classList += ' border-red';
-      break;
+//       //  if email is empty Email
+//       case 'auth/invalid-email':
+//       Swal.fire({
+//         icon: "error",
+//         title: "Error",
+//         text: "Email is Incorrect",
+//       });
+//       email.classList += ' border-red';
+//       break;
       
-      //  if email is empty Email
-      case 'auth/missing-password':
-        Swal.fire({
-          icon: "error",
-          title: "Error",
-          text: "Password is required",
-        });
-        password.classList += ' border-red';
-        break;
+//       //  if email is empty Email
+//       case 'auth/missing-password':
+//         Swal.fire({
+//           icon: "error",
+//           title: "Error",
+//           text: "Password is required",
+//         });
+//         password.classList += ' border-red';
+//         break;
 
-        default:
-          console.log(errorCode);
-          break;
-      }
-  });
-
-
-}
-signinBtn && signinBtn.addEventListener('click' , loginUser);
+//         default:
+//           console.log(errorCode);
+//           break;
+//       }
+//   });
 
 
-const forgotPassword = ()=>{
+// }
+// signinBtn && signinBtn.addEventListener('click' , loginUser);
 
-  sendPasswordResetEmail(auth, loginEmail.value)
-  .then(() => {
-    //Successfully Email sent
-    const Toast = Swal.mixin({
-      toast: true,
-      position: "top-end",
-      showConfirmButton: false,
-      timer: 3000,
-      timerProgressBar: true,
-      didOpen: (toast) => {
-        toast.onmouseenter = Swal.stopTimer;
-        toast.onmouseleave = Swal.resumeTimer;
-      },
-    });
-    Toast.fire({
-      icon: "success",
-      title: "Email has been Sent",
-    });
 
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
+// const forgotPassword = ()=>{
+
+//   sendPasswordResetEmail(auth, loginEmail.value)
+//   .then(() => {
+//     //Successfully Email sent
+//     const Toast = Swal.mixin({
+//       toast: true,
+//       position: "top-end",
+//       showConfirmButton: false,
+//       timer: 3000,
+//       timerProgressBar: true,
+//       didOpen: (toast) => {
+//         toast.onmouseenter = Swal.stopTimer;
+//         toast.onmouseleave = Swal.resumeTimer;
+//       },
+//     });
+//     Toast.fire({
+//       icon: "success",
+//       title: "Email has been Sent",
+//     });
+
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
     
-    Swal.fire({
-      icon: "error",
-      title: "Error",
-      text: `${errorCode}`,
-    });
-  });
-}
+//     Swal.fire({
+//       icon: "error",
+//       title: "Error",
+//       text: `${errorCode}`,
+//     });
+//   });
+// }
 
-forgetPassword.addEventListener('click', forgotPassword)
-//
+// forgetPassword.addEventListener('click', forgotPassword)
+// //
 
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
     
-    window.location.replace = "../Dashboard/dashboard.html"
+//     window.location.replace = "../Dashboard/dashboard.html"
 
-   // const uid = user.uid;
-    // ...
-  } else {
-    body.classList.add("slide"); 
-    window.location.href = "../Signup-Form/index.html"
-    // User is signed out
-    // ...
+//    // const uid = user.uid;
+//     // ...
+//   } else {
+//     body.classList.add("slide"); 
+//     window.location.href = "../Signup-Form/index.html"
+//     // User is signed out
+//     // ...
 
-  }
-});
+//   }
+// });
--- Signup-Form/signup.js
+++ Signup-Form/signup.js
@@ -1,45 +1,51 @@
-
-// //---------------- Replace the location to local host ----------------------- //
-// location.href.replace("http://localhost:5500/index.html");
-
 //--------------------------- importing ---------------------------------//
-import { auth, createUserWithEmailAndPassword, onAuthStateChanged } from "../firebase.js";
+// import { auth, createUserWithEmailAndPassword, onAuthStateChanged } from "../firebase.js";
 
-//-------------------------- Get Dom Elements ---------------------------//
-let signupbtn = document.getElementById("signup-btn");
-let backtosignupbtn = document.querySelector(".back-btn");
-let loginBtn = document.getElementById('login-btn')
 let body = document.querySelector(".body");
 body.classList.add("slide"); 
-console.log(body);
+
+//-------------------------- Get Dom Elements ---------------------------//
+let gotoSignup = document.getElementById("gotoSignup");
+let gotoLogin = document.getElementById("gotoLogin");
+let SignupButton = document.getElementById("SignupButton");
+let loginBtn = document.getElementById('login-btn')
+let forgetPass = document.getElementById('forgetPass')
 
   // get all inputs from Dom
   let firstName = document.getElementById("firstname");
   let lastName = document.getElementById("lastname");
-  let email = document.getElementById("email")
-  let password = document.getElementById("password")
+  let signupEmail = document.getElementById("email")
+  let signupPassword = document.getElementById("password")
   let birthdate = document.getElementById("birth-date");
   let address = document.getElementById("address");
+  let gendermale = document.getElementById("gendermale");
+  let genderfemale = document.getElementById("genderfemale");
+  let genderother = document.getElementById("genderother");
+  // Destructure gender in an array
+  let gender = Array.from([gendermale, genderfemale, genderother]);
+  
 
- //Make an object of sigup page inputs
-//  let inputValues = {
-//   userName : `${firstName.value} ${lastName.value}`,
-//   userEmail : email.value,
-//   userPassword : password.value,
-//   userAddress : address.value
-//  }
+//  Make an object of sigup page inputs
+ let inputValues = {
+  userName : `${firstName} ${lastName}`,
+  userEmail : signupEmail,
+  userPassword : signupPassword,
+  userAddress : address,
+  birthdate : birthdate,
+  gender : gender.find(item => item.checked)
+ }
 
   
   // ----------------------- add a slide in pages ---------------------------//
+  //Function of back button 
+  gotoSignup && gotoSignup.addEventListener('click' , ()=>{
+    body.classList.remove('slide');
+  });
  // move to login page
-loginBtn && loginBtn.addEventListener('click' , ()=>{
+gotoLogin && gotoLogin.addEventListener('click' , ()=>{
   body.classList.add("slide"); 
 });
 
-//Function of back button 
-backtosignupbtn && backtosignupbtn.addEventListener('click' , ()=>{
-  body.classList.remove('slide');
-});
 
 // ---------------------------Add a loader -----------------------//
 // const loader = ()=> {
@@ -54,103 +60,110 @@ backtosignupbtn && backtosignupbtn.addEventListener('click' , ()=>{
 //--------------------------- SignUp user-----------------------------//
 const signupUser = ()=>{
    
-    // if (email.value.trim() && password.value.trim()){
-    createUserWithEmailAndPassword(auth, email.value, password.value)
-    .then((userCredential) => {
-      // Signed up 
-      const user = userCredential.user;
-      const Toast = Swal.mixin({
-                  toast: true,
-                  position: "top-end",
-                  showConfirmButton: false,
-                  timer: 3000,
-                  timerProgressBar: true,
-                  didOpen: (toast) => {
-                    toast.onmouseenter = Swal.stopTimer;
-                    toast.onmouseleave = Swal.resumeTimer;
-                  },
-                });
-                Toast.fire({
-                  icon: "success",
-                  title: "Signed up successfully",
-                });
+//     // if (email.value.trim() && password.value.trim()){
+//     createUserWithEmailAndPassword(auth, email.value, password.value)
+//     .then((userCredential) => {
+//       // Signed up 
+//       const user = userCredential.user;
+//       const Toast = Swal.mixin({
+//                   toast: true,
+//                   position: "top-end",
+//                   showConfirmButton: false,
+//                   timer: 3000,
+//                   timerProgressBar: true,
+//                   didOpen: (toast) => {
+//                     toast.onmouseenter = Swal.stopTimer;
+//                     toast.onmouseleave = Swal.resumeTimer;
+//                   },
+//                 });
+//                 Toast.fire({
+//                   icon: "success",
+//                   title: "Signed up successfully",
+//                 });
 
-                setTimeout (()=>{
-                    body.classList.add("slide"); 
-                  },1000)
+//                 setTimeout (()=>{
+//                     body.classList.add("slide"); 
+//                   },1000)
       
-      console.log(user);
+//       console.log(user);
       
-      // ...
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
+//       // ...
+//     })
+//     .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
 
-        // Switch statements on error
-        switch (errorCode) {
-          //  if email is empty Email
-          case 'auth/missing-email':
-        Swal.fire({
-          icon: "error",
-          title: "Null",
-          text: "Email is required",
-        });
-        break;
+//         // Switch statements on error
+//         switch (errorCode) {
+//           //  if email is empty Email
+//           case 'auth/missing-email':
+//         Swal.fire({
+//           icon: "error",
+//           title: "Null",
+//           text: "Email is required",
+//         });
+//         break;
 
-        //  if email is empty Email
-          case 'auth/invalid-email':
-        Swal.fire({
-          icon: "error",
-          title: "Error",
-          text: "Email is Incorrect",
-        });
-        email.classList += ' border-red';
-        break;
+//         //  if email is empty Email
+//           case 'auth/invalid-email':
+//         Swal.fire({
+//           icon: "error",
+//           title: "Error",
+//           text: "Email is Incorrect",
+//         });
+//         email.classList += ' border-red';
+//         break;
 
-        //  if email is empty Email
-        case 'auth/email-already-in-use':
-        Swal.fire({
-          icon: "error",
-          title: "Error",
-          text: "Email is already registered",
-        });
-        email.classList += ' border-red'; 
-        break;
+//         //  if email is empty Email
+//         case 'auth/email-already-in-use':
+//         Swal.fire({
+//           icon: "error",
+//           title: "Error",
+//           text: "Email is already registered",
+//         });
+//         email.classList += ' border-red'; 
+//         break;
         
-        //  if email is empty Email
-        case 'auth/missing-password':
-          Swal.fire({
-            icon: "error",
-            title: "Null",
-            text: "Password is required",
-          });
-          password.classList += ' border-red';
-          break;
+//         //  if email is empty Email
+//         case 'auth/missing-password':
+//           Swal.fire({
+//             icon: "error",
+//             title: "Null",
+//             text: "Password is required",
+//           });
+//           password.classList += ' border-red';
+//           break;
 
-        default:
-          console.log(errorCode);
-          break;
-        }
-    });
+//         default:
+//           console.log(errorCode);
+//           break;
+//         }
+//     });
     
-// }
+// // }
+
+gender.map((el) => el.checked)
+console.log(gender);
+console.log(birthdate.value);
+console.log(...inputValues);
+
+
 
 }
-signupbtn && signupbtn.addEventListener('click' , signupUser);
+SignupButton && SignupButton.addEventListener('click' , signupUser);
 
-onAuthStateChanged(auth, (user) => {
-  if (user) {
+// onAuthStateChanged(auth, (user) => {
+//   if (user) {
     
-    body.classList.add('slide')
+//     body.classList.add('slide')
 
-    const uid = user.uid;
-    // ...
-  } else {
-    // User is signed out
-    // ...
-  }
-});
+//     const uid = user.uid;
+//     // ...
+//   } else {
+//     // User is signed out
+//     // ...
+//   }
+// });
 
 
 // If user is currently signed in
@@ -170,4 +183,3 @@ onAuthStateChanged(auth, (user) => {
 
 //
 
-export { body ,firstName , lastName , email , address}
